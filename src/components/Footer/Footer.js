f91b125abd4347ae7ff9ac65b5d917364332b127
Mahir-Asef <mahirasef30@gmail.com>
2022-03-29 20:04:13 +0600

    almost finished

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,174 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import footerLogo from "./../../assets/images/footer-logo.png";
+import Payment from "./../../assets/images/payment.png";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import "./footer.css";
+import {
+  faEnvelope,
+  faMapMarkerAlt,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebookF,
+  faYoutube,
+  faLinkedin,
+  faGithub
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { NavLink } from "react-router-dom";
+import FooterBG from "./../../assets/images/footer-bg.png";
+
+library.add(
+  faEnvelope,
+  faMapMarkerAlt,
+  faPhone,
+  faGithub,
+  faYoutube,
+  faLinkedin
+);
+const Footer = () => {
+  const navStyle = {
+    textDecoration: "none",
+    color: "white",
+    borderBottom: "2px solid transparent",
+    fontSize: "17px",
+    fontWeight: "bold",
+    marginBottom: "10px",
+    display: "inline-block",
+  };
+  return (
+    <div
+      style={{
+        background: `url(${FooterBG})`,
+        backgroundRepeat: "repeat",
+        padding: "40px 0 0",
+      }}
+    >
+      <Container>
+        <Row>
+          <Col>
+            <div className="text-center">
+              <img width="120px" src={footerLogo} alt="" />
+            </div>
+            <ul className="list-unstyled mt-3 w-100">
+              <li className="fs-6 fw-bold mb-2 text-white">
+                <FontAwesomeIcon
+                  className="me-2 social-icon"
+                  icon={faMapMarkerAlt}
+                />{" "}
+                Mirpur-1,Dhaka,Bangladesh
+              </li>
+              <li className="fs-6 fw-bold mb-2 text-white">
+                <FontAwesomeIcon
+                  className="me-2 social-icon"
+                  icon={faEnvelope}
+                />{" "}
+                Official:mahirasef30@gmail.com
+              </li>
+              <li className="fs-6 fw-bold mb-2 text-white">
+                <FontAwesomeIcon className="me-2 social-icon" icon={faPhone} />{" "}
+                Helpline: 01521309037(Available:10:00AM to 10.00PM)
+              </li>
+            </ul>
+          </Col>
+          <Col md={3}>
+            <ul className="list-unstyled">
+              <li>
+                <NavLink className="menuStyle" style={navStyle} to="/home">
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="menuStyle" style={navStyle} to="/about">
+                  About us
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="menuStyle" style={navStyle} to="/contact">
+                  Contact us
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink className="menuStyle" style={navStyle} to="/courses">
+                  Courses
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink className="menuStyle" style={navStyle} to="/policy">
+                  Policy
+                </NavLink>
+              </li>
+            </ul>
+          </Col>
+          <Col md={4}>
+            <img className="img-fluid" src={Payment} alt="" />
+          </Col>
+        </Row>
+        <div>
+          <h4 className="text-white text-center">Follow Us</h4>
+          <ul className="list-unstyled d-flex justify-content-center">
+            <li className="social-link">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.facebook.com/profile.php?id=100010043595086"
+              >
+                <FontAwesomeIcon
+                  style={{ color: "#3b5998" }}
+                  className="me-2 facebook social-icon"
+                  icon={faFacebookF}
+                />
+              </a>
+            </li>
+            <li className="social-link">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/Mahir-Asef"
+              >
+                <FontAwesomeIcon
+                  className="me-2 youtube social-icon"
+                  icon={faGithub}
+                />
+              </a>
+            </li>
+            <li className="social-link">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/mahirashef/"
+              >
+                <FontAwesomeIcon
+                  className="me-2 linkedin social-icon"
+                  icon={faLinkedin}
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </Container>
+      <hr className="mt-2 mb-0 bg-white" />
+      <p
+        style={{ background: "#000099" }}
+        className="m-0 fw-bold py-3  text-white text-center"
+      >
+        Copyright &copy; All Reserved by{" "}
+        <a
+          className="text-decoration-none"
+          style={{ color: "#ff136f" }}
+          target="_blank"
+          rel="noreFerrer"
+          href="https://www.facebook.com/groups/codingclubpro2021"
+        >
+          DEV COURSES programming community
+        </a>{" "}
+        in 2021
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCNb97gIqSpj5Mbl0ufP9eghBmmvVyucdo",
+  authDomain: "dev-courses-d0971.firebaseapp.com",
+  projectId: "dev-courses-d0971",
+  storageBucket: "dev-courses-d0971.appspot.com",
+  messagingSenderId: "949127561586",
+  appId: "1:949127561586:web:65faf3dd36ab94d6669deb",
+  measurementId: "G-XNMS3BDMGB"
+};
+
+export default firebaseConfig;
